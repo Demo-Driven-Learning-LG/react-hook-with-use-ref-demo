4e6e3596290b110fa50ba8ad882e9c2adb9055bb
Le Gao <le.lgao@compass.com>
2023-12-31 15:18:27 +0800

    Set webpack config and create index.js

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
